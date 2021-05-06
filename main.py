0958827e76de9e26d2afc8c44642c81c6591a6f4
cgrebosky <cgrebosky@gmail.com>
2021-05-06 09:30:34 -0700

    make program main

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import datetime
+import os
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# From https://www.quora.com/Whats-the-best-spaced-repetition-schedule.  Not really scientific, but it seems decent.
+schedule = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    today = datetime.date.today()
+    schedule_days = [today - datetime.timedelta(days=i) for i in schedule]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    schedule_days = [i.strftime("%m/%d") for i in schedule_days]
+    output_string = ", ".join(schedule_days)
+
+    # God, applescript is literally the worst :'(.  Why does this exist?  Who thought this was a good idea?
+    # Also, you'll have to change this for a different OS, but this should work on pretty much all OSX versions
+    os.system("osascript -e 'display notification \"%s\" with title \"Spaced Repetition Reminder\"\'" % output_string)
